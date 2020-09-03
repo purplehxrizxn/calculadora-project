bc71207e8e050b0d6816cddfab7a7f8fb403665e
Panza <gabrielpanzeri@live.com>
2020-09-03 10:59:03 -0300

    get e set

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,3 +1,24 @@
 class CalcController {
-    
+    constructor(){
+
+        // _ é a convenção para atributo privado
+        this._displayCalc = '0'
+        this._dataAtual
+
+    }
+
+    get displayCalc(){
+        return this._displayCalc
+    }
+
+    set displayCalc(valor){
+        this._displayCalc = valor
+    }
+
+    get dataAtual(){
+        return this._dataAtual
+    }
+    set dataAtual(valor){
+        this._dataAtual = valor
+    }
 }
