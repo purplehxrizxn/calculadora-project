52545ced6d39e8d599631e00ab902bbd355afd42
Panza <gabrielpanzeri@live.com>
2020-09-04 11:31:49 -0300

    added dot function

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,6 +2,8 @@ class CalcController {
     constructor(){
 
         // _ é a convenção para atributo privado
+        this._lastOperator = ''
+        this._lastNumber = ''
         this._operation = [];
         this._locale = 'pt-BR'
         this._displayCalcEl = document.querySelector("#display")
@@ -18,6 +20,7 @@ class CalcController {
         setInterval(() =>{
             this.setDisplayDateTime()
         }, 1000)
+        this.setLastNumberToDisplay()
     }
 
     addEventListenerAll(element, events, fn){
@@ -28,11 +31,15 @@ class CalcController {
 
     clearAll(){
         this._operation = []
+        this._lastNumber = ''
+        this._lastOperator = ''
+        this.setLastNumberToDisplay()
 
     }
 
     clearEntry(){
         this._operation.pop()
+        this.setLastNumberToDisplay()
 
     }
 
@@ -61,29 +68,79 @@ class CalcController {
 
     }
 
+    getResult(){
+
+
+
+        return eval(this._operation.join(""))
+
+    }
+
     calc(){
 
-        let last = this._operation.pop()
-        // join é o contrário do split, ele junta usando um caractere
-        let result = eval(this._operation.join(""))
+        let last = ''
 
-        this._operation = [result, last]
+        this._lastOperator = this.getLastItem()
+
+        if(this._operation.length < 3) {
+            let firstItem = this._operation[0]
+            this._operation = [firstItem, this._lastOperator, this._lastNumber]
+        }
+
+        if(this._operation.length > 3){
+            last = this._operation.pop()
+            this._lastNumber = this.getResult()
+
+        }else if(this._operation.length == 3){
+            this._lastNumber = this.getLastItem(false)
+        }
+
+        // join é o contrário do split, ele junta usando um caractere
+        let result = this.getResult()
+
+        if (last == '%'){
+
+            result /= 100
+
+            this._operation = [result]
+
+        } else {
+
+            this._operation = [result]
+
+        if(last) this._operation.push(last)
+
+        }
 
         this.setLastNumberToDisplay()
 
     }
 
-    setLastNumberToDisplay(){
+    getLastItem(isOperator = true){
 
-        let lastNumber;
+        let lastItem;
 
         for(let i = this._operation.length-1; i >= 0; i--){
 
-            if(!this.isOperator(this._operation[i])){
-                lastNumber = this._operation[i]
+            if(this.isOperator(this._operation[i]) == isOperator){
+                lastItem = this._operation[i]
                 break;
             }
         }
+
+        if(!lastItem){
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber
+        }
+
+        return lastItem
+    }
+
+    setLastNumberToDisplay(){
+
+        let lastNumber = this.getLastItem(false);
+
+        if(!lastNumber) lastNumber = 0
+
         this.displayCalc = lastNumber
     }
 
@@ -95,8 +152,6 @@ class CalcController {
 
                 this.setLastOperation(value)
 
-            } else if (isNaN(value)){
-                console.log(value)
             } else {
                 this.pushOperation(value)
 
@@ -112,7 +167,7 @@ class CalcController {
             } else {
 
                 let newValue = this.getLastOperation().toString() + value.toString()
-                this.setLastOperation(parseInt(newValue))
+                this.setLastOperation(newValue)
 
                 //atualizar display
                 this.setLastNumberToDisplay()
@@ -124,6 +179,23 @@ class CalcController {
 
     setError(){
         this.displayCalc = 'Error'
+    }
+
+    addDot(){
+
+        let lastOperation = this.getLastOperation()
+
+        if(typeof lastOperation === 'string' && lastOperation.split('').indexOf('.')) return
+
+        if (this.isOperator(lastOperation || !lastOperation)){
+
+            this.pushOperation('0.')
+        } else {
+            this.setLastOperation(lastOperation.toString() + '.')
+        }
+
+        this.setLastNumberToDisplay()
+
     }
 
     execBtn(value){
@@ -157,11 +229,11 @@ class CalcController {
                 break;
 
             case 'igual':
-
+                this.calc()
                 break;
 
             case 'ponto':
-                this.addOperation('.')
+                this.addDot('.')
                 break;
 
             case '0':
